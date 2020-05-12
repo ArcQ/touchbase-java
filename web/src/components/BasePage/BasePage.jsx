1e28b7b4a9f4cc0b5c2ab9d501177391aee99079
Alanna Tai <alanna.tai@hotmail.com>
2020-05-11 20:15:05 -0400

    Implement redux into existing app

--- web/src/components/BasePage/BasePage.jsx
+++ web/src/components/BasePage/BasePage.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './BasePage.css';
 
-const BasePage = ({ bases, selectedBase }) => {
+function BasePage({ selectedBase }) {
+  if (selectedBase) {
+    return (
+      <div className='BasePage'>
+        <p className='BasePage-name'>{selectedBase.name}</p>
+        <p className='BasePage-score'>
+          <span class='fa fa-star yellow'></span> {selectedBase.score}
+        </p>
+        <img src={selectedBase.icon} alt={selectedBase.name} />
+      </div>
+    );
+  }
   return (
     <div className='BasePage'>
-      <p className='BasePage-name'>{bases[selectedBase].name}</p>
-      <p className='BasePage-score'>
-        <span class='fa fa-star yellow'></span> {bases[selectedBase].score}
-      </p>
-      <img src={bases[selectedBase].icon} alt={bases[selectedBase].name} />
+      <p className='BasePage-name'>Welcome to TouchBase</p>
     </div>
   );
+}
+
+// const BasePage = ({ bases, selectedBase }) => {
+//   return (
+//     <div className='BasePage'>
+//       <p className='BasePage-name'>{bases[selectedBase].name}</p>
+//       <p className='BasePage-score'>
+//         <span class='fa fa-star yellow'></span> {bases[selectedBase].score}
+//       </p>
+//       <img src={bases[selectedBase].icon} alt={bases[selectedBase].name} />
+//     </div>
+//   );
+// };
+
+const mapStateToProps = (state) => {
+  return {
+    selectedBase: state.selectedBase,
+  };
 };
 
-export default BasePage;
+export default connect(mapStateToProps)(BasePage);
--- web/src/components/NavBases/NavBases.jsx
+++ web/src/components/NavBases/NavBases.jsx
@@ -1,22 +1,63 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
+import { selectBase } from '../../actions';
 import './NavBases.css';
 
-const NavBases = ({ bases, selectedBase, handleBaseClick }) => {
+function NavBases({ bases, selectedBase, selectBase }) {
+  //force coding this for now, will refactor
+  if (selectedBase) {
+    return (
+      <div className='NavBases'>
+        {bases.map((base) => (
+          <div
+            key={base.name}
+            id={selectedBase.name === base.name ? 'selected' : ''}
+            className='NavBasesItem'
+            onClick={() => selectBase(base)}
+          >
+            <img className='circle-icon' src={base.icon} alt={base.name} />
+          </div>
+        ))}
+      </div>
+    );
+  }
   return (
     <div className='NavBases'>
-      {bases.map((base, i) => (
+      {bases.map((base) => (
         <div
-          key={i}
-          id={selectedBase === i ? 'selected' : ''}
+          key={base.name}
           className='NavBasesItem'
-          onClick={() => handleBaseClick(i)}
+          onClick={() => selectBase(base)}
         >
           <img className='circle-icon' src={base.icon} alt={base.name} />
         </div>
       ))}
     </div>
   );
+}
+
+// const NavBases = ({ bases, selectedBase, handleBaseClick }) => {
+//   return (
+//     <div className='NavBases'>
+//       {bases.map((base, i) => (
+//         <div
+//           key={i}
+//           id={selectedBase === i ? 'selected' : ''}
+//           className='NavBasesItem'
+//           onClick={() => handleBaseClick(i)}
+//         >
+//           <img className='circle-icon' src={base.icon} alt={base.name} />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+const mapStateToProps = (state) => {
+  return {
+    bases: state.bases,
+    selectedBase: state.selectedBase,
+  };
 };
 
-export default NavBases;
+export default connect(mapStateToProps, { selectBase })(NavBases);
