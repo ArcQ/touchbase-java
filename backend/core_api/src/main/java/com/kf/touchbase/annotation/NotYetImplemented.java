918f29bbe72dde5c69f7256da0695c94e8e23544
Eddie Law <eddielaw296@gmail.com>
2020-07-17 01:44:20 -0400

    feat: add not implemented yet

---
+++ backend/core_api/src/main/java/com/kf/touchbase/annotation/NotYetImplemented.java
@@ -0,0 +1,19 @@
+package com.kf.touchbase.annotation;
+
+import com.kf.touchbase.aop.NotYetImplementedInterceptor;
+import io.micronaut.aop.Around;
+import io.micronaut.context.annotation.Type;
+import io.micronaut.core.annotation.Internal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//TODO extend this with api doc and restrict on controller level, attaching to http annotation
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Around
+@Type(NotYetImplementedInterceptor.class)
+@Internal
+public @interface NotYetImplemented {}
--- backend/core_api/src/main/java/com/kf/touchbase/rest/BaseController.java
+++ backend/core_api/src/main/java/com/kf/touchbase/rest/BaseController.java
@@ -1,8 +1,9 @@
 package com.kf.touchbase.rest;
 
-import com.kf.touchbase.models.domain.postgres.Success;
+import com.kf.touchbase.annotation.NotYetImplemented;
 import com.kf.touchbase.mappers.BaseMapper;
 import com.kf.touchbase.models.domain.neo4j.Base;
+import com.kf.touchbase.models.domain.postgres.Success;
 import com.kf.touchbase.models.dto.BaseReq;
 import com.kf.touchbase.services.neo4j.BaseService;
 import com.kf.touchbase.utils.AuthUtils;
@@ -11,6 +12,7 @@ import io.micronaut.http.annotation.*;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.authentication.Authentication;
 import io.micronaut.security.rules.SecurityRule;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
@@ -41,18 +43,34 @@ public class BaseController {
 
     @Post("/{baseId}/owner/{authId}")
     @Produces(MediaType.APPLICATION_JSON)
+    @NotYetImplemented
+    @Operation(description = "Not Implemented Yet")
     public Base addUserToBaseAsOwner(Authentication authentication, String baseId, String authId) {
         return baseService.addUserToBaseAsOwner(AuthUtils.getUserIdFromAuth(authentication), authId, baseId);
     }
 
+    @Patch("/{baseId}/owner/{authId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @NotYetImplemented
+    @Operation(description = "Not Implemented Yet")
+    public Base patchUserToBaseAsOwner(Authentication authentication, String baseId, String authId, @Body BaseReq baseReq) {
+        UUID baseUuid = UUID.fromString(baseId);
+        var base = baseMapper.baseReqToBase(baseReq);
+        return baseService.patchBase(AuthUtils.getUserIdFromAuth(authentication), baseUuid, base);
+    }
+
     @Delete("/{baseId}/owner/{authId}")
     @Produces(MediaType.APPLICATION_JSON)
+    @NotYetImplemented
+    @Operation(description = "Not Implemented Yet")
     public Base removeUserFromBaseAsOwner(Authentication authentication, String baseId, String authId) {
         return baseService.deleteUserFromBaseAsOwner(AuthUtils.getUserIdFromAuth(authentication), authId, baseId);
     }
 
     @Delete("/{baseId}")
     @Produces(MediaType.APPLICATION_JSON)
+    @NotYetImplemented
+    @Operation(description = "Not Implemented Yet")
     public Success makeBaseInactive(Authentication authentication, UUID baseId) {
         return baseService.makeBaseInactive(AuthUtils.getUserIdFromAuth(authentication), baseId);
     }
