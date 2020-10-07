28c0cbb629ab5993d4e0bb4a5a6d94961f35b46b
ArcQ <eddielaw296@gmail.com>
2020-10-07 00:42:55 -0400

    feat: modify kafka config

--- src/main/java/com/kf/touchbase/services/MissionCommandImpl.java
+++ src/main/java/com/kf/touchbase/services/MissionCommandImpl.java
@@ -1,44 +1,47 @@
 package com.kf.touchbase.services;
 
+import com.kf.touchbase.models.domain.MissionTemplate;
 import com.kf.touchbase.models.domain.postgres.Mission;
+import com.kf.touchbase.models.domain.postgres.User;
+import com.kf.touchbase.repository.BaseMemberRepository;
 import com.kf.touchbase.repository.MissionRepository;
-import com.kf.touchbase.repository.UserRepository;
+import io.micronaut.scheduling.annotation.Scheduled;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Singleton;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Singleton
 @Slf4j
 public class MissionCommandImpl {
 
-    UserRepository userRepository;
+    BaseMemberRepository baseMemberRepository;
     MissionRepository missionRepository;
 
+   private List<MissionTemplate> generateMissionTemplates(User user) {
+       return Collections.singletonList(new MissionTemplate());
+   }
+
     //twice a week
-    //    @Scheduled(cron = "0 12 0 0 1,4")
-    void executeTwiceAWeek() {
+    @Scheduled(cron = "0 12 0 0 1,4")
+    void setMissions() {
         log.info("Twice a week job: {}",
                 new SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(new Date()));
-    }
-
-    //weekly
-    //    @Scheduled(cron = "0 12 0 0 2")
-    //    void setMissions() {
-    //        LOG.info("Starting to set missions | once a week job: {}",
-    //                new SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(new Date()));
-    //        Pageable pageRequest = Pageable.from(0, 200);
-    //        Page<User> onePage = userRepository.findAll(pageRequest);
-    //
-    //        while (!onePage.isEmpty()) {
-    //            pageRequest = pageRequest.next();
-    //            onePage.forEach(user ->);
-    //            onePage = userRepository.findAll(pageRequest);
-    //        }
-    //    }
-
-    Mission addProgress() {
-        return null;
+        baseMemberRepository.findAll()
+                .flatMapIterable((baseMember) -> generateMissionTemplates(baseMember.getUser())
+                        .stream()
+                        .map((missionTemplate) ->
+                                Mission.builder()
+                                        .assignedUser(baseMember.getUser())
+                                        .assignedBase(baseMember.getBase())
+                                        .storedMissionTemplate(missionTemplate)
+                                        .build())
+                        .collect(Collectors.toList()))
+                .buffer(5)
+                .flatMap(missionRepository::saveAll).subscribe();
     }
 }
